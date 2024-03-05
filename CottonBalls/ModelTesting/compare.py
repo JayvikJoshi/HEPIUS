61b0ede69fbcc15f5f6e1232268c48ab6d24c52f
Jayvik Joshi <jayvik.joshi@gmail.com>
2024-03-05 12:22:37 -0500

    added display function to iterate through images

--- CottonBalls/ModelTesting/compare.py
+++ CottonBalls/ModelTesting/compare.py
@@ -1,5 +1,7 @@
 import heapq
 import cv2
+import os
+
 
 def get_info(img_path, target_path, pred_path):
     image = cv2.imread(img_path)
@@ -27,20 +29,20 @@ def get_info(img_path, target_path, pred_path):
     return image, target_boxes, pred_boxes
 
 
-def draw_bounding_boxes(image, target_boxes, pred_boxes):
+def draw_bounding_boxes(image, target_boxes, pred_boxes, iou):
+    font = cv2.FONT_HERSHEY_SIMPLEX
     for box in target_boxes:
         x1, y1, x2, y2 = box
         cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
     for box in pred_boxes:
         x1, y1, x2, y2 = box
         cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
-    cv2.imshow('image', image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    cv2.putText(image, f"IoU: {iou}", (10, 30), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
+    return image
 
 def compute_iou(target_boxes, pred_boxes):
 
-    iou_list = []
+    iou = []
     for box1 in target_boxes:
         for box2 in pred_boxes:
             
@@ -48,7 +50,7 @@ def compute_iou(target_boxes, pred_boxes):
             x1_box2, y1_box2, x2_box2, y2_box2 = box2
 
             if x1_box1 > x2_box2 or x2_box1 < x1_box2 or y1_box1 > y2_box2 or y2_box1 < y1_box2:
-                iou_list.append(0)
+                iou.append(0)
             else:
                 x_left = max(x1_box1, x1_box2)
                 x_right = min(x2_box1, x2_box2)
@@ -58,15 +60,39 @@ def compute_iou(target_boxes, pred_boxes):
                 intersection_area = (x_right - x_left) * (y_bottom - y_top)
                 box1_area = (x2_box1 - x1_box1) * (y2_box1 - y1_box1)
                 box2_area = (x2_box2 - x1_box2) * (y2_box2 - y1_box2)
-                iou = intersection_area / (box1_area + box2_area - intersection_area)
-                iou_list.append(round(iou, 3))
-    print(iou_list)
-    print(heapq.nlargest(len(target_boxes), iou_list))
+                iou_val = intersection_area / (box1_area + box2_area - intersection_area)
+                iou.append(round(iou_val, 3))
+    return heapq.nlargest(len(target_boxes), iou)
+
+def display(img_folder, target_folder, pred_folder):
+
+    for img_file in os.listdir(img_folder):
+        if img_file.endswith(".png"):
+            img_path = os.path.join(img_folder, img_file)
+            target_path = os.path.join(target_folder, "target_" + img_file)
+            pred_path = os.path.join(pred_folder, "predicted_" + img_file)
+            
+            image, target_boxes, pred_boxes = get_info(img_path, target_path, pred_path)
+            iou = compute_iou(target_boxes, pred_boxes)
+            while True:
+                bb_image = draw_bounding_boxes(image.copy(), target_boxes, pred_boxes, iou)
+                cv2.imshow("Bounding Box Image", bb_image)
+                #either quit or save image
+                key = cv2.waitKey(0)
+                if key == ord('q'):
+                    break
+                elif key == ord('s'):
+                    cv2.imwrite(os.path.join(img_folder, "bb_" + img_file), bb_image)
+                    break
+            cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
-    img_path = "/Users/jayvik/Desktop/img.png"
-    pred_txt = "/Users/jayvik/Desktop/pred.txt"
-    target_txt = "/Users/jayvik/Desktop/targ.txt"
-    image, target_boxes, pred_boxes = get_info(img_path, target_txt, pred_txt)
-    #draw_bounding_boxes(image, target_boxes, pred_boxes)
-    compute_iou(target_boxes, pred_boxes)
+    # img_path = "/Users/jayvik/Desktop/img.png"
+    # pred_txt = "/Users/jayvik/Desktop/pred.txt"
+    # target_txt = "/Users/jayvik/Desktop/targ.txt"
+    # image, target_boxes, pred_boxes = get_info(img_path, target_txt, pred_txt)
+    # bb_image = draw_bounding_boxes(image, target_boxes, pred_boxes)
+    # iou = compute_iou(target_boxes, pred_boxes)
+
+    
