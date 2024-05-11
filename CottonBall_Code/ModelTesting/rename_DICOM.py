d43b27648d250d687470e4c3148d69c50e3de380
Jayvik Joshi <jayvik.joshi@gmail.com>
2024-05-11 11:34:12 -0400

    added method to rename dicoms to new image IDs

--- CottonBall_Code/ModelTesting/rename_DICOM.py
+++ CottonBall_Code/ModelTesting/rename_DICOM.py
@@ -1,10 +1,25 @@
-#goal: take a folder of DICOM files and match them to .png files by name, then rename the DICOM files to match the .png files
-#input: folder of DICOM files, folder of .png files
-#output: DICOM files renamed to match .png files
-
 import os
-import pydicom
-import shutil
+import pandas as pd
 
-#path to DICOM files
-dicom_path = "/Users/jayvik/Documents/GitHub/HEPIUS/CottonBall_Data/
+original_dicoms = 1
+original_pngs = '/Users/jayvik/Desktop/CottonBall_Data/original_pngs'
+excel_path = '/Users/jayvik/Desktop/CottonBall_Data/cottonball_info.xlsx'
+
+df = pd.read_excel(excel_path)
+
+for subdir, _, files in os.walk(original_dicoms):
+    subfolder = os.path.basename(subdir)
+    r = df.index[df["Subfolder"] == subfolder].tolist()
+    if r != []:
+        r = r[0]
+    
+        filepaths_list = []
+        for file in files:
+            filepaths_list.append(os.path.join(subdir, file))
+        filepaths_list.sort()
+        
+        i = 0
+        for filepath in filepaths_list:
+            num_string = f"{i:04d}"
+            os.rename(filepath, f"{subdir}/IMG_{df['ID'][r]}_{num_string}")
+            i += 1
